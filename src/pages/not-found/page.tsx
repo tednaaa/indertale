56cae287ff194ef52ba046e6f2f0d7fbe94a802d
Andranik <tednaaa@gmail.com>
2023-10-29 21:42:32 +0400

    fix: not found page back link & colors

--- src/pages/not-found/page.tsx
+++ src/pages/not-found/page.tsx
@@ -1,9 +1,11 @@
+import { Link } from 'react-router-dom';
+
 import { routes } from '@/shared/routes';
-import { Link } from '@/shared/ui/link';
+import { Container } from '@/shared/ui/container';
+
+import styles from './styles.module.scss';
 
 import astronautsImage from './assets/astronauts.svg';
-import styles from './styles.module.scss';
-import { Container } from '@/shared/ui/container';
 
 export const NotFoundPage = () => {
   return (
@@ -17,7 +19,9 @@ export const NotFoundPage = () => {
               Page Not Found
             </h1>
             <p className={styles.text}>Sorry about that! Please visit our homepage to get where you need to go.</p>
-            <Link to={routes.HOME}>Back to homepage</Link>
+            <Link className={styles.backLink} to={routes.HOME}>
+              Back to homepage
+            </Link>
           </div>
           <img className={styles.image} src={astronautsImage} alt="404 not found" />
         </div>
